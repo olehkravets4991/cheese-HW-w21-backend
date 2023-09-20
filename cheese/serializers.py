5de5a5821d62f5341fb025acfcc2fc980bdca351
Oleh <kravetsoleh4991@gmail.com>
2023-09-19 17:29:39 -0700

    cheese3

--- cheese/serializers.py
+++ cheese/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 ## Class TodoSerializer is a subclass of serializers.HyperlinkedModelSerializer.
 ## For serializing and deserializing data into representations such as json.
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
+class CheeseSerializer(serializers.HyperlinkedModelSerializer):
     ## Meta class is for configuring the TodoSerializer class.
     class Meta:
         # model to serialize
--- cheeseproject/urls.py
+++ cheeseproject/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from cheeses.views import CheeseViewSet
+from cheese.views import CheeseViewSet
 
 # create a new router
 router = routers.DefaultRouter()
